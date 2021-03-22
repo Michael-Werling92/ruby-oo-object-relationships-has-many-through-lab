46fe891469d5afc8f55127ad49c58eb5ed8c275e
Michael <mfw6mw@virginia.edu>
2021-03-21 20:26:51 -0400

    commit tree f00de1f0d99b5c993c82057e90bc52b966eec8f2

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,27 @@
+class Appointment
+
+    attr_accessor :doctor, :date, :patient
+
+    @@all = [ ]
+
+    def initialize (date, patient, doctor)
+        @date = date
+        @doctor = doctor
+        @patient = patient
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def doctor
+        @doctor
+    end
+
+    def patient
+        @patient
+    end
+
+end
 
